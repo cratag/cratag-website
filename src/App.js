9ff3256fc58e418ad56306b705f508d7c356a087
cratag <cratag@hotmail.com>
2020-12-12 09:29:40 -0300

    update

--- src/App.js
+++ src/App.js
@@ -2,21 +2,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div className="App">
+  <div className="Header"> 
+<p>Cratag</p>
+</div>
+<div className="Body">
+<p>Asi que chuchuchuchupelo</p>
+</div>
+</div>
   );
 }
 
