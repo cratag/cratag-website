93fe7d4e7320a58603ad227264ae7350ddcc9427
cratag <cratag@hotmail.com>
2020-12-16 19:07:23 -0300

    object banner, link to home, testing layout start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, {useState} from 'react';
+import {BrowserRouter as Router,Switch,Route, Link} from "react-router-dom";
+
 import './App.css';
 import mail from "./assets/mail.png"
 import git from "./assets/git2.png"
+import template from "./assets/template.png"
 
-import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 
 
 export default function App() {     
@@ -27,33 +29,62 @@ return (
   );
 }
 
-function Home() {
+function Header(){
   const [gitGlow, setGitGlow] = useState(false);
   const [mailGlow, setMailGlow] = useState(false);
   return <div>
-
-  <div className="Header">
+{/* HEADER */}
+<div className="Header">
   <div style={{padding: "0.5%"}}>
-  <p className="HeaderTitle">crat<p style={{display: 'inline', fontFamily: 'Calibri'}}>λ</p>g</p>
-    <div style={{marginRight: "2%", display: "inline", float: "right", color: "black"}}> - </div>
-    <a href="mailto:devcratag@gmail.com"><img className="HeaderButton" src={mail} alt="Mail" style={{height: mailGlow ? "30px" : "25px"}} onMouseEnter={() => setMailGlow(true)} onMouseLeave={() => setMailGlow(false)} /></a>
-    <a href="https://www.github.com/cratag"><img className="HeaderButton" src={git} alt="Git" style={{height: gitGlow ? "30px" : "25px"}} onMouseEnter={() => setGitGlow(true)} onMouseLeave={() => setGitGlow(false)} /></a>
+  <Link to="/" style={{ color: '#FFF', textDecoration: "none" }}><p className="HeaderTitle">crat<p style={{display: 'inline', fontFamily: 'Calibri'}}>λ</p>g</p></Link>
+  <Link to="/about">Yeah</Link>
+      {/* SPACE */}<div style={{marginRight: "2%", display: "inline", float: "right", color: "black"}}> - </div>
+
+      {/* MAIL */}
+    <a href="mailto:devcratag@gmail.com"><img 
+      className="HeaderButton" src={mail} 
+      alt="Mail" style={{height: mailGlow ? "30px" : "25px"}} 
+      onMouseEnter={() => setMailGlow(true)} 
+      onMouseLeave={() => setMailGlow(false)} />
+    </a>
+
+      {/* GIT */}
+    <a href="https://www.github.com/cratag"><img 
+      className="HeaderButton" src={git} 
+      alt="Git" style={{height: gitGlow ? "30px" : "25px"}} 
+      onMouseEnter={() => setGitGlow(true)} 
+      onMouseLeave={() => setGitGlow(false)} />
+    </a>
   </div> 
-    
+</div>
+{/* HEADER END */}
+
   </div>
-  
-  <div className="Container">
-    <div className="CenterBody">
-    <text className="test"> Hello w2orld</text>
-    </div>
-  
+}
+
+function Home() {
+
+return <div>
+{Header()}
+
+{/* CONTAINER */}
+<div>
+  <div className="CenterBody">
+    <img  src={template} className="CenterBodyImage" alt="temp"/>
+    <img  src={template} className="CenterBodyImage" alt="temp"/>
+    <img  src={template} className="CenterBodyImage" alt="temp"/>
   </div>
-  </div>
-  ;};
+</div>
+{/* CONTAINER END*/}
+
+</div>;};
 
 
 function About() {
-  return <h2>About</h2>;
+  return <div>
+{Header()}
+<h1>Voila it works</h1>
+</div>;
 }
 
 function Users() {
