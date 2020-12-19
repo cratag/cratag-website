745cb9f61a5e13b38aff2798abac8fb700ae1ab6
cratag <cratag@hotmail.com>
2020-12-19 19:08:09 -0300

    layout def

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, {useState} from 'react';
 import {BrowserRouter as Router,Switch,Route, Link} from "react-router-dom";
 
+import useWindowDimensions from "./dimensions"
+
 import './App.css';
 import mail from "./assets/mail.png"
 import git from "./assets/git2.png"
-import template from "./assets/template.png"
+import contact from "./assets/contact.png"
+import projects from "./assets/projects.png"
+import aboutme from "./assets/aboutme.png"
 
 
 
@@ -14,24 +18,35 @@ return (
 <Router>
       <div>
         <Switch>
+
+          <Route path="/projects">
+            <Projects />
+          </Route>
+
+          <Route path="/contact">
+            <Contact />
+          </Route>
+
           <Route path="/about">
             <About />
           </Route>
+
           <Route path="/">
             <Home />
           </Route>
+
+
         </Switch>
       </div>
     </Router>
-  );
-}
+);}
 
+// HEADER
 function Header(){
   const [gitGlow, setGitGlow] = useState(false);
   const [mailGlow, setMailGlow] = useState(false);
-  return <div>
+  return <div className="Header">
 {/* HEADER */}
-<div className="Header">
   <div style={{padding: "0.5%"}}>
   <Link to="/" style={{ color: '#FFF', textDecoration: "none" }}><p className="HeaderTitle">crat<p style={{display: 'inline', fontFamily: 'Calibri'}}>λ</p>g</p></Link>
   {/* <Link to="/about">Yeah</Link> */}
@@ -53,49 +68,59 @@ function Header(){
       onMouseLeave={() => setGitGlow(false)} />
     </a>
   </div> 
-</div>
 {/* HEADER END */}
-
-  </div>
+</div>
 }
+// HEADER END
+
+// FOOTER
+function Footer(){
+  return <div className="Footer">
+<p> Copyright (c) 2020 Copyright <a style={{ color: '#FFF', textDecoration: "none" }} href="http://www.cratag.com/">cratag.com</a> All Rights Reserved.</p>
+</div>
+}
+// FOOTER END
+
 
 function Home() {
+  const { width, height } = useWindowDimensions();
+  let imageWidth = width * 0.15
+  let heightAsMargin = height * 0.13
 return <div>
 {Header()}
-
 {/* CONTAINER */}
-  <div className="CenterBodyOuter">
+  <div className="CenterBodyOuter" style={{marginTop: heightAsMargin}}>
       <center>
     <div className="CenterBody">
-        <img className="CenterBodyImage" src={template}  alt="temp"/>
-        <img className="CenterBodyImage" src={template}  alt="temp"/>
-        <img className="CenterBodyImage" src={template}  alt="temp"/>
+        <Link to="/contact"><img className="CenterBodyImage" style={{width: imageWidth}} src={contact}  alt="Contact"/></Link>
+        <Link to="/projects"><img className="CenterBodyImage" style={{width: imageWidth}} src={projects}  alt="Projects"/></Link>
+        <Link to="/about"><img className="CenterBodyImage" style={{width: imageWidth}} src={aboutme}  alt="About Me"/></Link>
     </div>
       </center>
   </div>
-  
 {/* CONTAINER END*/}
+
+{Footer()}
 </div>;};
 
+
+function Projects() {
+  return <div>
+{Header()}
+<h1 style={{margin: "20%", color: 'white'}}>PROJECTS</h1>
+{Footer()}
+</div>;}
 
 function About() {
   return <div>
 {Header()}
-<h1>Voila it works</h1>
-</div>;
-}
+<h1 style={{margin: "20%", color: 'white'}}>About</h1>
+{Footer()}
+</div>;}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function Contact() {
+  return <div>
+{Header()}
+<h1 style={{margin: "20%", color: 'white'}}>About</h1>
+{Footer()}
+</div>;}
